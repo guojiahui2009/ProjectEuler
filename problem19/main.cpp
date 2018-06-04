60228ceea7d5de800d9c0c926d577aaa99a7136c
guojiahui2009 <guojiahui2009@gmail.com>
2018-06-03 22:29:22 -0400

    test

---
+++ problem19/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  problem19
+//
+//  Created by Jiahui Guo on 6/2/18.
+//  Copyright © 2018 projectEuler. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    
+    cout << "Hello Hello\n";
+    
+    
+    return 0;
+}
